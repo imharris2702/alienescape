e600dc5fed89e68727cd735eb58a6efe9f7c0ece
Isaiah Harris <69725417+imharris2702@users.noreply.github.com>
2022-10-21 16:08:08 -0400

    Enemy patrol state implemented
    currently doesn't reenter patrol state once engaged

--- Scripts/AI.gd
+++ Scripts/AI.gd
@@ -3,6 +3,7 @@ extends Node2D
 signal state_changed(new_state)
 
 onready var player_detection_zone = $PlayerDetectionZone
+onready var patrol_timer = $PatrolTimer
 
 export (int) var player_idle_distance = 120
 export (int) var player_shoot_distance = 120
@@ -13,17 +14,27 @@ enum State {
 }
 
 var current_state: int = State.PATROL setget set_state
-var player = null
-var actor = null
+var actor: KinematicBody2D = null
+
+#Engage State vars
+var player: Player = null
+
+#Patrol state vars
+var origin: Vector2 = Vector2.ZERO
+var patrol_location: Vector2 = Vector2.ZERO
+var patrol_location_reached: bool = true
 
 func _process(delta):
-	if actor == null:
-		print("Error: no enemy")
 	if actor.isDead:
 		return
 	match current_state:
 		State.PATROL:
-			pass
+			if !patrol_location_reached:
+				actor.move_and_slide(actor.velocity)
+				if actor.global_position.distance_to(patrol_location) < 5:
+					patrol_location_reached = true
+					actor.velocity = Vector2.ZERO
+					patrol_timer.start()
 		State.ENGAGE:
 			if player != null:
 				var direction: Vector2 = player.position - actor.position
@@ -41,10 +52,15 @@ func _process(delta):
 
 func initialize(actor):
 	self.actor = actor;
+	origin = actor.global_position
+	patrol_timer.start()
 
 func set_state(new_state: int):
 	if new_state == current_state:
 		return
+	if new_state == State.PATROL:
+		origin = actor.global_position
+		patrol_timer.start()
 	current_state = new_state
 	emit_signal("state_changed", current_state)
 
@@ -52,3 +68,12 @@ func _on_PlayerDetectionZone_body_entered(body: Node):
 	if body.is_in_group("player"):
 		set_state(State.ENGAGE)
 		player = body
+
+
+func _on_PatrolTimer_timeout():
+	var patrol_range = 50
+	var random_x = rand_range(-patrol_range, patrol_range)
+	var random_y = rand_range(-patrol_range, patrol_range)
+	patrol_location = Vector2(random_x, random_y) + origin
+	patrol_location_reached = false
+	actor.velocity = actor.global_position.direction_to(patrol_location) * actor.movespeed
