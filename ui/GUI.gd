fcb94e93cd4ebcad1cb6e2a07a4f54fe9091669d
Maddman0 <maddman2001@gmail.com>
2022-10-21 04:19:54 -0400

    added death vignette

--- ui/GUI.gd
+++ ui/GUI.gd
@@ -5,10 +5,9 @@ onready var health_tween = $MarginContainer/Rows/TopRow/HealthSection/HealthTwee
 var original_color = Color("#0bbe03")
 var highlight_color = Color("#a2d79e")
 
-onready var death_vignette = $DeathVignette
+onready var death_vignette = $DeathVignette.material
 onready var death_tween = $DeathVignette/DeathTween
 var vignette_color = Color('#961919')
-var max_player_health: int
 
 var player
 
@@ -24,9 +23,16 @@ func set_new_health_value(new_health: int):
 	health_tween.interpolate_property(bar_style, "bg_color", original_color, highlight_color, 0.2, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	health_tween.interpolate_property(bar_style, "bg_color", highlight_color, original_color, 0.2, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0.2)
 	health_tween.start()
-	if new_health <= 0:
-		pass
+#	if new_health <= 0:
+#		print('death tween start')
+#		death_tween.targeting_method()
 		
 func _process(delta: float) -> void:
-	#death_vignette.get_material().set_shader_param("multiplier", death_vignette.get_material().get_shader_param("multiplier") - .01)
-	pass
+	if player.isDead and death_vignette.get_shader_param("multiplier") > -.3:
+		death_vignette.set_shader_param("multiplier", death_vignette.get_shader_param("multiplier") - .01)
+		death_vignette.set_shader_param("softness", death_vignette.get_shader_param("softness") - .001)
+
+
+#func _on_DeathTween_tween_step(object: Object, key: NodePath, elapsed: float, value: Object) -> void:
+#	print("death tween step called")
+#	death_vignette.material.set_shader_param("multiplier", death_vignette.get_material().get_shader_param("multiplier") - .01)
