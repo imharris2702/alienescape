a245b8262326d5f3008b97f518cb00c1bbd44af7
Maddman0 <maddman2001@gmail.com>
2022-10-23 02:13:56 -0400

    fixed jail cell z_index

--- Scripts/StartingArea.gd
+++ Scripts/StartingArea.gd
@@ -15,4 +15,5 @@ func _process(delta: float) -> void:
 	if Time.get_ticks_msec() / 1000 > timeClosed:
 		animation_player.play("Off")
 		lasers.set_deferred("disabled", true)
-		
+		$Cell/laser1.z_index = -1
+		$Cell/laser2.z_index = -1
